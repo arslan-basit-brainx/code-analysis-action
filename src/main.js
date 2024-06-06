9eb9acb43d7c17d77802161d49c9c8ee3c950838
arslan-basit-brainx <arslan.basit@brainxtech.com>
2024-06-06 17:42:12 +0500

    changes

--- src/main.js
+++ src/main.js
@@ -8,20 +8,8 @@ const fs = require('fs')
  */
 async function run() {
   try {
-    // The `who-to-greet` input is defined in action metadata file
-    //const whoToGreet = core.getInput('who-to-greet', { required: true })
-    //core.info(`Hello, ${whoToGreet}!`)
 
-    // Get the current time and set as an output
-    //const time = new Date().toTimeString()
-    //core.setOutput('time', time)
-
-    // Output the payload for debugging
-    // core.info(
-    //   `The event payload: ${JSON.stringify(github.context.payload, null, 2)}`
-    // )
-
-    var text = fs.readFileSync("./report.txt");
+    let text = fs.readFileSync('./report.txt')
     core.info(`Text from file ${text}`)
   } catch (error) {
     // Fail the workflow step if an error occurs
