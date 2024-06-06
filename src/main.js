f8955e2e539a1b81c57bdd942e211f0442eda46c
arslan-basit-brainx <arslan.basit@brainxtech.com>
2024-06-06 17:28:20 +0500

    Read report

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
+const fs = require('fs')
 
 /**
  * The main function for the action.
@@ -8,17 +9,20 @@ const github = require('@actions/github')
 async function run() {
   try {
     // The `who-to-greet` input is defined in action metadata file
-    const whoToGreet = core.getInput('who-to-greet', { required: true })
-    core.info(`Hello, ${whoToGreet}!`)
+    //const whoToGreet = core.getInput('who-to-greet', { required: true })
+    //core.info(`Hello, ${whoToGreet}!`)
 
     // Get the current time and set as an output
-    const time = new Date().toTimeString()
-    core.setOutput('time', time)
+    //const time = new Date().toTimeString()
+    //core.setOutput('time', time)
 
     // Output the payload for debugging
-    core.info(
-      `The event payload: ${JSON.stringify(github.context.payload, null, 2)}`
-    )
+    // core.info(
+    //   `The event payload: ${JSON.stringify(github.context.payload, null, 2)}`
+    // )
+
+    var text = fs.readFileSync("./report.txt");
+    core.info(`Text from file ${text}`)
   } catch (error) {
     // Fail the workflow step if an error occurs
     core.setFailed(error.message)
